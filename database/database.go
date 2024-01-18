5855f24b5e39f17522f9a35b358113046762c0dc
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-01-18 14:20:18 -0300

    Add business logic to initial db config

--- database/database.go
+++ database/database.go
@@ -14,10 +14,22 @@ var (
 func Connect() {
 	connection := "host=localhost user=root password=root dbname=videoVault port=5432 sslmode=disable"
 
-	DB, err = gorm.Open(postgres.Open(connection))
+	DB, err = gorm.Open(postgres.Open(connection), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
+
 	if err != nil {
 		panic("could not connect to the database")
 	}
 
-	DB.AutoMigrate(&models.Video{})
+	DB.AutoMigrate(&models.Video{}, &models.Category{})
+
+	baseConfig()
+}
+
+func baseConfig() {
+	DB.FirstOrCreate(&models.Category{
+		Title: "Livre",
+		Color: "#FFF",
+	})
 }
